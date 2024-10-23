fd5981f9da545eb1a35bac37cd46ea96e2491b22
kevin-3552 <efiratcan@hotmail.com>
2024-10-23 18:45:10 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
     // Temel sahne, kamera ve renderer
     const scene = new THREE.Scene();
-    const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+    const camera = new THREE.PerspectiveCamera(30, window.innerWidth / window.innerHeight, 0.1, 1000);
     const renderer = new THREE.WebGLRenderer({ antialias: true });
     renderer.setSize(window.innerWidth, window.innerHeight);
     renderer.shadowMap.enabled = true; // Gölgelendirme etkinleştir
